d217464834c755193f3c5acd0d28c458d9e408a5
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2026-08-30 19:19:56 +0600

    solve

--- components/dashboard-content.tsx
+++ components/dashboard-content.tsx
@@ -443,7 +443,7 @@ export function DashboardContent() {
 
     useEffect(() => {
         const initStripe = async () => {
-            const stripeInstance = await loadStripe('pk_test_51T28pm7kECw44sgCk3RxtMKst01YwUY02L1R93SaiJVPHloYcnWAar0NytN5TcduerTeWbS1yRa0hJehyB7N2JSC00WWO8y9aa');
+            const stripeInstance = await loadStripe('pk_live_51T28pa5zlEk8k5ZE1lbopSBYCwSD9Q8zSAxwbscuXBkqkpCvCyiuom6TrkZNxq22vBtUIbxQGZpzMc39q3xXxpo700Ylnyg6YU');
             setStripe(stripeInstance);
         };
         initStripe();
