f5bb5c386cdf5f3bc45eb447e13bbe24fc326a6c
Md. Nazim Ahmed <77225770+nazimnk9@users.noreply.github.com>
2026-09-02 17:52:18 +0600

    solve

--- components/billing-content.tsx
+++ components/billing-content.tsx
@@ -495,7 +495,7 @@ export function BillingContent({ blockedStep = null }: BillingContentProps) {
                     'Authorization': `Bearer ${token}`
                 },
                 body: JSON.stringify({
-                    topup_minute: parseInt(topUpMinutes),
+                    quantity: parseInt(topUpMinutes),
                     payment_method_id: selectedPmForTopUp.id
                 })
             });
